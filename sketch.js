2a528194200c6b3b31b07b6769744c795288ac46
redspydernebula <65370988+redspydernebula@users.noreply.github.com>
2022-01-06 08:57:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,19 +13,4 @@ function draw() {
   }
 }
 
-function myFunction() {
-  if (a < 10) {
-    print("Yessss....");
-  } else if (a < 50) {
-    print("I crave clicks");
-  } else if (a < 100) {
-    if (a % 2 == 0) print("You have done well fledgling clicker");
-    else print("I am not yet satiated");
-  } else if (a < 200) {
-    print("Your hand may hurt but I must feed");
-  }
-
-  a++;
-}
-
 
