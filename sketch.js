032eb8e5df596e8b8710a6df86b2246c32405677
redspydernebula <65370988+redspydernebula@users.noreply.github.com>
2022-01-07 08:26:11 -0500

    Changed title
    Changed "Welcome to my website" to "welcome to my portfolio."

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
   background(0,0,0,0);
-  for(let i = 0; i < 22; i++){
+  for(let i = 0; i < 24; i++){
     let a = sin(3.2*((abs(i*25 - frameCount)%255)/255));
     document.getElementById("t" + i).style.color="rgba(" + 250*a + ",250,150," + a + ")";
   }
